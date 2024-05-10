eb1341b26ae6c858f2078c70426cb092a998a7ab
rdftgyhhuy76 <js1kekeme13@hotmail.com>
2024-05-10 21:14:44 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function countAndSay(n) {
-  let result = "1";
-  for (let i = 1; i < n; i++) {
-    let temp = "";
-    let count = 1;
-    for (let j = 0; j < result.length; j++) {
-      if (result[j] === result[j + 1]) {
-        count++;
-      } else {
-        temp += count + result[j];
-        count = 1;
-      }
-    }
-    result = temp;
-  }
-  return result;
-}
+const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => !s.has(x));
+};
